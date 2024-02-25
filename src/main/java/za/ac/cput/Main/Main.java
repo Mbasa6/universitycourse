5b96d693b6aec1150eef4d2acc57694cc746fd74
Mlungisi <mmbuyazi@gmail.com>
2024-02-25 20:29:28 +0200

    Lecture Class Test

--- src/main/java/za/ac/cput/Main/Main.java
+++ src/main/java/za/ac/cput/Main/Main.java
@@ -1,8 +1,13 @@
 package za.ac.cput.Main;
 
+import za.ac.cput.Domain.Lecture;
+
 public class Main {
     public static void main(String[] args) {
 
+        Lecture lectuer1 = new Lecture("Le01", "Mlungisi", "Mbuyazi", "Male", "mmbuyazi63@gmail.com", "0739956042");
+        System.out.println(lectuer1);
+
     }
-    
+
 }
