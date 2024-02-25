22642a9486dee8bdf111c20abac20dbe3f2ef056
Mpumzi <mphumzimbula@gmail.com>
2024-02-25 21:21:45 +0200

    Add Student pojo and updated Main class

--- src/main/java/za/ac/cput/Domain/Student.java
+++ src/main/java/za/ac/cput/Domain/Student.java
@@ -1,4 +1,92 @@
 package za.ac.cput.Domain;
 
+import java.time.LocalDate;
+
 public class Student {
+
+
+    private  String email;
+    private String phoneNumber;
+    private String studentName;
+    private String studentSurname;
+    private long idNumber;
+    private int studentNumber;
+    private LocalDate dateOfBirth;
+    private Contact contact;
+
+    public Student() {
+        contact= new Contact();
+    }
+
+    public Student( String studentName, String studentSurname, long idNumber, int studentNumber, LocalDate dateOfBirth,String email, String phoneNumber) {
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.studentName = studentName;
+        this.studentSurname = studentSurname;
+        this.idNumber = idNumber;
+        this.studentNumber = studentNumber;
+        this.dateOfBirth = dateOfBirth;
+        contact=new Contact(phoneNumber,email);
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentSurname() {
+        return studentSurname;
+    }
+
+    public void setStudentSurname(String studentSurname) {
+        this.studentSurname = studentSurname;
+    }
+
+    public long getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(int idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public int getStudentNumber() {
+        return studentNumber;
+    }
+
+    public void setStudentNumber(int studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Contact getContact() {
+        return contact;
+    }
+
+    public void setContact(Contact contact) {
+        this.contact = contact;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+
+                ", studentName=" + studentName + '\'' +
+                ", studentSurname='" + studentSurname + '\'' +
+                ", idNumber=" + idNumber +
+                ", studentNumber=" + studentNumber +
+                ", dateOfBirth=" + dateOfBirth +
+                ", " + contact +
+                '}';
+    }
 }
--- src/main/java/za/ac/cput/Main/Main.java
+++ src/main/java/za/ac/cput/Main/Main.java
@@ -1,9 +1,29 @@
 package za.ac.cput.Main;
 
+import za.ac.cput.Domain.Course;
 import za.ac.cput.Domain.Lecture;
+import za.ac.cput.Domain.Student;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
+
+        String dObString = "2000-Jun-20";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MMM-dd");
+        formatter = formatter.withLocale(Locale.UK);  // Locale specifies human language for translating, and cultural norms for lowercase/uppercase and abbreviations and such. Example: Locale.US or Locale.CANADA_FRENCH
+        LocalDate date = LocalDate.parse( dObString, formatter);
+
+//Im creating a new student Object and setting its values
+        Student student1 = new Student("Mpumzi", "Mbula", 00062, 219053324, date, "mphumzimbula@gmail.com", "0658436358");
+        System.out.println(student1);
+
+        Course course1 = new Course();
+        course1.studentList.add(student1);
+
+
 
         Lecture lectuer1 = new Lecture("Le01", "Mlungisi", "Mbuyazi", "Male", "mmbuyazi63@gmail.com", "0739956042");
         System.out.println(lectuer1);
